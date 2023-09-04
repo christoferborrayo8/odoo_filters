0ffc189e9c4a43681f8485e39a9cda9d23659b3d
Christofer Borrayo <christoferborrayo8@gmail.com>
2023-09-04 13:04:49 -0600

    feat: Añadida configuración inicial de docker de odoo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# odoo_filters
+# Filtros en Odoo 
+
+Módulo de ejemplo para visualizar los distintos tipos de filtros que podemos aplicar en odoo.
+
+---
+## Pasos para ejecutar el contendor
+### Dar permisos al setup.sh 
+```sudo chmod +x setup.sh```
+### Ejecutar setup.sh 
+```./setup.sh```
+
+---
+# Autor
+- **Christofer William Borrayo López**
+- *@christoferborrayo8*
---
+++ addons/Modulo_personal/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Personal',
+    'summary': 'Módulo para ejemplificar filtros con un modelo de Personal.',
+    'description': 'Este módulo será utilizado para poder visualizar los distintos tipos de filtros que pueden aplicarse a una vista',
+    'version': '1.0.0',
+    'category': 'Human Resources',
+    'author': 'Christofer Borrayo',
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
