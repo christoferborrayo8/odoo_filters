8fe0aec9bca3b830ff170ad3d21f06268353c3d8
Christofer Borrayo <christoferborrayo8@gmail.com>
2023-09-04 16:34:03 -0600

    feat: Agregado filtros con dominio y actions

--- addons/Modulo_personal/models/Personal.py
+++ addons/Modulo_personal/models/Personal.py
@@ -7,3 +7,16 @@ class Personal(models.Model):
     codigo          = fields.Integer(string = 'Codigo')
     nombre          = fields.Char(string = 'Nombre del empleado')
     edad            = fields.Integer(string = 'Edad')
+
+    def filtro_ver_el_personal(self):
+        tree_id = self.env.ref("Modulo_personal.personal_view_tree")
+        domain = ['|',('edad','<=','27'),('categoria.codigo','=','03')]
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Vista de personal (Con actions)',
+            'view_mode': 'tree',
+            'res_model': 'personal',
+            'domain': domain,
+            'views': [(tree_id.id, 'tree')],
+            'target': 'current',
+        }
